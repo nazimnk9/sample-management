d9e1567e19089060104c4179162e266c0a137443
Md. Nazim Ahmed <77225770+nazimnk9@users.noreply.github.com>
2026-01-01 12:51:30 +0600

    remove extra line

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-// import { redirect } from "next/navigation"
-
-// export default function Home() {
-//   redirect("/dashboard")
-// }
-
 import { redirect } from "next/navigation"
 
 export default function Home() {
